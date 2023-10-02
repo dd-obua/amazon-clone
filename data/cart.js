809662b21ae8b197b7e79e14bdbc9aa18d9f3459
Obua Denis Daniel <dd.obua@gmail.com>
2023-10-02 13:55:55 +0300

    Generate HTML for order summary

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,8 @@
 import { select } from '../scripts/select.js';
-export const cart = [];
+export const cart = [
+  { pdtId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', pdtQnty: 2 },
+  { pdtId: '15b6fc6f-327a-4ec4-896f-486349e85a3d', pdtQnty: 1 },
+];
 
 let pdtCount = 0;
 
